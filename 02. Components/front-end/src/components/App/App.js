820dc6f62282756b3687909d9c7d2cc379016a9e
Nikolay Nikolaev <n.nikolaevbg97@gmail.com>
2019-02-19 18:02:28 +0200

    Front-End Initial Setup.

---
+++ 02. Components/front-end/src/components/App/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react"; 
+import "./App.css";
+
+class App extends Component {
+  componentWillMount() {
+    fetch("http://localhost:9999/feed/street/all")
+      .then(rawData => rawData.json())
+      .then(data => console.log(data));
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;

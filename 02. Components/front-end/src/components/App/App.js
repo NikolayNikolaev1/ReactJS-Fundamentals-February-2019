20c7ad523995b736282581db9fb8b4677ff60b00
Nikolay Nikolaev <n.nikolaevbg97@gmail.com>
2019-02-19 19:40:23 +0200

    Finished Exercise.

--- 02. Components/front-end/src/components/App/App.js
+++ 02. Components/front-end/src/components/App/App.js
@@ -1,28 +1,81 @@
-import React, { Component } from "react"; 
+import React, { Component } from "react";
 import "./App.css";
+import Street from "../Street/Street";
+import House from "../House/House";
+import HouseDetails from "../HouseDetails/HouseDetails";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      streets: [],
+      selectedStreetId: 0,
+      selectedHouseId: 0,
+      hasFetched: false
+    };
+  }
   componentWillMount() {
     fetch("http://localhost:9999/feed/street/all")
       .then(rawData => rawData.json())
-      .then(data => console.log(data));
+      .then(data =>
+        this.setState({
+          streets: data.streets,
+          hasFetched: true
+        })
+      );
+  }
+  getStreets() {
+    return this.state.streets;
+  }
+  getSelectedStreetHouses() {
+    if (this.state.hasFetched) {
+      return this.state.streets[this.state.selectedStreetId].homes;
+    }
+
+    return [];
+  }
+  setCurrentStreet(index) {
+    this.setState({
+      selectedStreetId: index
+    });
+  }
+  setCurrentHouse(index) {
+    this.setState({
+      selectedHouseId: index
+    });
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="streets">
+          {this.getStreets().map((street, index) => (
+            <Street
+              selectStreet={e => this.setCurrentStreet(index, e)}
+              location={street.location}
+              ket={index}
+              id={index}
+            />
+          ))}
+        </div>
+        <div className="houses">
+          {this.getSelectedStreetHouses()
+            ? this.getSelectedStreetHouses().map((house, index) => (
+                <House
+                  selectHouse={e => this.setCurrentHouse(index, e)}
+                  imageUrl={house.imageUrl}
+                  ket={index}
+                  id={index}
+                />
+              ))
+            : ""}
+        </div>
+        {this.state.hasFetched ? (
+          <HouseDetails
+            house={this.getSelectedStreetHouses()[this.state.selectedHouseId]}
+          />
+        ) : (
+          ""
+        )}
       </div>
     );
   }

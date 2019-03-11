94cb705b272c83124e3e332da287b12b057a594c
Nikolay Nikolaev <n.nikolaevbg97@gmail.com>
2019-03-11 20:00:52 +0200

    Finish working on this.

--- 06. Workshop/client/src/App.js
+++ 06. Workshop/client/src/App.js
@@ -3,18 +3,21 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./views/home";
 import NotFound from "./views/not-found";
 import Login from "./views/login";
+import CreateBook from "./views/create-book";
+import Logout from './views/logout';
 import Header from "./components/header";
 import Footer from "./components/footer";
 import {
   UserProvider,
   defaultUserState
 } from "./components/contexts/user-context";
+import AuthorizedRoute from "./components/authorized-route";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    const userFromStorate = window.localStorage.getItem('user');
+    const userFromStorate = window.localStorage.getItem("user");
     let parsedUser = userFromStorate ? JSON.parse(userFromStorate) : {};
 
     this.state = {
@@ -42,6 +45,12 @@ class App extends Component {
               <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/login" component={Login} />
+                <AuthorizedRoute
+                  path="/create-book"
+                  component={CreateBook}
+                  allowedRoles={["admin"]}
+                />
+                <AuthorizedRoute path="/logout" component={Logout} />
                 <Route component={NotFound} />
               </Switch>
               <Footer />
